49453f04f4537c2df6d18dba0d76202957159daa
Natein <natein@gmail.com>
2020-02-20 22:17:04 +0200

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let res = 0;
+    while(n != 0){
+        let dig = n % 10;
+        res = res * 10 + dig;
+        n = Math.trunc(n / 10);        
+    }
+    return res;  
 }
